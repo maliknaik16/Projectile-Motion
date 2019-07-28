fdfc30c217c43edd430753ae4988ef76e2a99890
Malik Naik <maliknaik16@gmail.com>
2019-07-28 22:42:40 +0530

    Implmenet ProjectileMotion.cs

---
+++ Assets/_Scripts/ProjectileMotion.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+namespace ProjectileMotion
+{
+  public class ProjectileMotion
+  {
+    // Horizontal distance of the object travelled with the projectile.
+    public static float HorizontalDistance(float initialVelocity, float angle, float time)
+    {
+      return initialVelocity * Mathf.Cos(angle) * time;
+    }
+
+    public static float AscentTime(float initialVelocity, float angle, float acceleration) {
+      return initialVelocity * Mathf.Sin(angle) / acceleration;
+    }
+
+    public static float DescentTime(float initialVelocity, float angle, float acceleration) {
+      return ProjectileMotion.AscentTime(initialVelocity, angle, acceleration);
+    }
+
+    public static float FlightTime(float initialVelocity, float angle, float acceleration) {
+      return 2f * ProjectileMotion.AscentTime(initialVelocity, angle, acceleration);
+    }
+  }
+}
